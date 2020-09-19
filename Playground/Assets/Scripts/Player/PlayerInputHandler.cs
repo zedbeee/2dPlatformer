36e50e00bf2c74546c5612d9b778e9dd0b280f29
zedbeee <30415091+zedbeee@users.noreply.github.com>
2020-09-18 20:25:03 -0400

    Fixing double jump buffer thing

--- Playground/Assets/Scripts/Player/PlayerInputHandler.cs
+++ Playground/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -9,7 +9,13 @@ public class PlayerInputHandler : MonoBehaviour
     public int NormInputX {get; private set;}
     public int NormInputY {get; private set;}
     public bool JumpInput{get; private set;}
+    [SerializeField]
+    private float inputHoldTime = 0.2f;
+    private float jumpInputStartTime;
 
+    private void Update() {
+        CheckJumpInputHoldTime();
+    }
     public void OnMoveInput(InputAction.CallbackContext context){
         RawMovementInput = context.ReadValue<Vector2>();
         NormInputX = (int)(RawMovementInput*Vector2.right).normalized.x;
@@ -19,6 +25,7 @@ public class PlayerInputHandler : MonoBehaviour
     public void onJumpInput(InputAction.CallbackContext context){
         if (context.started){
             JumpInput = true;
+            jumpInputStartTime = Time.time;
         }
         if (context.performed){
             //Jump being held
@@ -29,4 +36,9 @@ public class PlayerInputHandler : MonoBehaviour
     }
 
     public void UseJumpInput() => JumpInput = false;
+    private void CheckJumpInputHoldTime(){
+        if (Time.time >= jumpInputStartTime + inputHoldTime){
+            JumpInput = false;
+        }
+    }
 }
